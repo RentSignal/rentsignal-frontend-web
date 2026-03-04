4fa1fc7a8e41651616edf414a431aedae73fa9e3
WOOJIN CHOI <dnwls0723@sookmyung.ac.kr>
2026-03-04 15:27:27 +0900

    chore: 법정동 구역별 색상 constant 정의

--- src/constants/regionColor.ts
+++ src/constants/regionColor.ts
@@ -1,13 +1,14 @@
+// 색상은 일단 임시로 둠
 export const regionColor: Record<string, string> = {
-  도심권: "#87BFFF", // 선명하고 신뢰감 있는 스카이 블루 (포인트)
-  동북권: "#B8D7F2", // 부드러운 파스텔 블루
-  서북권: "#D9EAFD", // 가장 밝고 투명한 아이스 블루
-  서남권: "#A6C1EE", // 차분하고 보랏빛이 살짝 섞인 페리윙클 블루
-  동남권: "#7AA2CE", // 깊이감 있는 차분한 미디엄 블루
+  도심권: "#87BFFF",
+  동북권: "#B8D7F2",
+  서북권: "#D9EAFD",
+  서남권: "#A6C1EE",
+  동남권: "#7AA2CE",
 };
 
 export const regionBoundaryColor: Record<string, string> = {
-  도심권: "#638BB8", // 면보다 한 톤 낮은 경계선
+  도심권: "#638BB8",
   동북권: "#638BB8",
   서북권: "#638BB8",
   서남권: "#638BB8",
