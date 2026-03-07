863fed65ba4f33d21d0b39cb323eda80cecc1690
WOOJIN CHOI <dnwls0723@sookmyung.ac.kr>
2026-03-07 19:11:24 +0900

    feat: 정보 탭 토글에 지수 설명 popover 추가

--- src/components/CategoryToggle.tsx
+++ src/components/CategoryToggle.tsx
@@ -1,7 +1,24 @@
 import { useDragScroll } from "@/hooks/useDragScroll";
+import PopOverIcon from "@/assets/icons/popover_icon.svg?react";
+import {
+  Popover,
+  PopoverContent,
+  PopoverDescription,
+  PopoverHeader,
+  PopoverTitle,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+
+type CategoryItem = {
+  id: string;
+  label: string;
+  showPopover?: boolean;
+  title?: string;
+  description?: string;
+};
 
 type CategoryToggleProps = {
-  items: string[];
+  items: CategoryItem[];
   value: string;
   onChange: (value: string) => void;
 };
@@ -10,9 +27,9 @@ const CategoryToggle = ({ items, value, onChange }: CategoryToggleProps) => {
   const { ref, isDragging, onMouseDown, shouldBlockClick } =
     useDragScroll<HTMLDivElement>();
 
-  const onClickItem = (item: string) => {
+  const onClickItem = (itemId: string) => {
     if (shouldBlockClick()) return;
-    onChange(item);
+    onChange(itemId);
   };
 
   return (
@@ -20,8 +37,7 @@ const CategoryToggle = ({ items, value, onChange }: CategoryToggleProps) => {
       ref={ref}
       onMouseDownCapture={onMouseDown}
       className={`
-        flex 
-        flex-nowrap items-center 
+        flex flex-nowrap items-center
         gap-2 pr-4
         overflow-x-auto no-scrollbar
         select-none
@@ -29,25 +45,44 @@ const CategoryToggle = ({ items, value, onChange }: CategoryToggleProps) => {
       `}
     >
       {items.map((item) => (
-        <button
-          key={item}
-          draggable={false}
-          onClick={() => onClickItem(item)}
-          className={`
-            shrink-0 whitespace-nowrap 
-            px-3 py-1 
-            rounded-md 
-            text-sm font-medium 3
-            transition-all
-            ${
-              value === item
-                ? "bg-blue-90 text-coolNeutral-10 border border-blue-90"
-                : "text-[#626363] border border-gray-300"
-            }
-          `}
-        >
-          {item}
-        </button>
+        <div key={item.id} className="flex items-start gap-1 shrink-0">
+          <button
+            draggable={false}
+            onClick={() => onClickItem(item.id)}
+            className={`
+        whitespace-nowrap
+        px-3 py-1
+        rounded-md
+        text-sm font-medium
+        transition-all
+        ${
+          value === item.id
+            ? "bg-blue-90 text-coolNeutral-10 border border-blue-90"
+            : "text-[#626363] border border-gray-300"
+        }
+      `}
+          >
+            {item.label}
+          </button>
+
+          {item.showPopover && (
+            <Popover>
+              <PopoverTrigger asChild>
+                <PopOverIcon className="w-3 h-3" />
+              </PopoverTrigger>
+              <PopoverContent
+                align="start"
+                className="p-4 bg-white shadow-lg outline-none w-72 rounded-xl ring-0"
+              >
+                <PopoverHeader>
+                  <PopoverTitle className="pb-3">{item.title}</PopoverTitle>
+
+                  <PopoverDescription>{item.description}</PopoverDescription>
+                </PopoverHeader>
+              </PopoverContent>
+            </Popover>
+          )}
+        </div>
       ))}
     </div>
   );
