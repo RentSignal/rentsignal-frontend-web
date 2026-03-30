31698ef93d8d04db60bd92a2ba1724cd22181ad6
WOOJIN CHOI <dnwls0723@sookmyung.ac.kr>
2026-03-30 19:36:26 +0900

    refactor: 탭 네비게이션용 InfoSectionToggle 컴포넌트 추가 - 기존 state 타입을 number에서 string으로 변경

---
+++ src/components/Info/section/InfoSectionToggle.tsx
@@ -0,0 +1,33 @@
+type InfoSectionToggleType = "INFO" | "LIFESTYLE";
+type Props = {
+  value: InfoSectionToggleType;
+  onChange: (value: InfoSectionToggleType) => void;
+};
+
+export default function InfoSectionToggle({ value, onChange }: Props) {
+  return (
+    <div className="flex pl-5 gap-[12px] mb-2 sticky top-0 bg-white z-20 pt-2">
+      <button
+        onClick={() => onChange("INFO")}
+        className={`text-lg font-semibold transition-colors pb-1 ${
+          value === "INFO"
+            ? "text-blue-60 border-b-2 border-blue-60"
+            : "text-toggle_grey border-b-2 border-transparent"
+        }`}
+      >
+        지수 중심
+      </button>
+
+      <button
+        onClick={() => onChange("LIFESTYLE")}
+        className={`text-lg font-semibold transition-colors pb-1 ${
+          value === "LIFESTYLE"
+            ? "text-blue-60 border-b-2 border-blue-60"
+            : "text-toggle_grey border-b-2 border-transparent"
+        }`}
+      >
+        생활요소 중심
+      </button>
+    </div>
+  );
+}
