312840e5551f1a7cee19710c2f9d09f5cb78a406
WOOJIN CHOI <dnwls0723@sookmyung.ac.kr>
2026-04-12 12:03:11 +0900

    feat: 편의시설 > 우선순위 분기 처리 기능 구현

--- src/components/recommend/steps/Step3.tsx
+++ src/components/recommend/steps/Step3.tsx
@@ -10,17 +10,27 @@ export default function Step3({
   onSubmit: () => void;
 }) {
   const preferOption = ["가성비", "편의시설"];
+  const facilityOption = [
+    "편의점",
+    "카페",
+    "병원",
+    "약국",
+    "음식점",
+    "대형마트",
+    "교통",
+    "치안",
+  ];
   const isValid = value.length > 0;
 
   return (
     <div className="pt-[17px] px-5 flex flex-col gap-6">
       <div>
         <h2 className="text-base font-semibold">Step 3</h2>
-        <p className="text-sm text-gray-500">
+        <p className="text-sm font-medium text-coolNeutral-30">
           우선순위로 둘 생활요소를 선택하세요.
         </p>
 
-        <div className="flex gap-2 mt-3">
+        <div className="flex gap-[5px] mt-3">
           {preferOption.map((item) => {
             const selected = value.includes(item);
 
@@ -29,7 +39,7 @@ export default function Step3({
                 key={item}
                 onClick={() => onChange(item)}
                 className={`
-                  px-3 py-2 border rounded-lg
+                  flex-1 px-3 py-2 border rounded-lg
                   ${
                     selected
                       ? "bg-blue-60 text-white border-blue-60"
@@ -42,6 +52,14 @@ export default function Step3({
             );
           })}
         </div>
+        {value === "편의시설" && (
+          <div className="pt-[48px] mt-3">
+            {/* TODO: 편의시설 우선순위 UI 들어갈 자리 */}
+            <p className="text-sm font-medium text-coolNeutral-25">
+              드래그하여 우선순위를 정해주세요.
+            </p>
+          </div>
+        )}
       </div>
 
       <div className="flex justify-end gap-1">
