74cdcc325c0861b010ee479a8fc229811b01a2d4
WOOJIN CHOI <dnwls0723@sookmyung.ac.kr>
2026-04-12 09:50:57 +0900

    feat: 검색어 초기화 버튼 추가 및 기능 구현

--- src/components/recommend/steps/Step1.tsx
+++ src/components/recommend/steps/Step1.tsx
@@ -1,4 +1,5 @@
 import SearchIcon from "@/assets/icons/search_icon.svg?react";
+import CloseIcon from "@/assets/icons/close_icon.svg?react";
 import regions from "@/data/regions.json";
 import { useState, useEffect } from "react";
 
@@ -51,7 +52,7 @@ export default function Step1({
       <div className="flex flex-col gap-[207px]">
         <div className="relative">
           <span className="absolute -translate-y-1/2 left-3 top-1/2">
-            <SearchIcon></SearchIcon>
+            <SearchIcon />
           </span>
           <input
             className={`
@@ -76,13 +77,30 @@ export default function Step1({
             }}
             placeholder="거주하고자 하는 희망 지역을 검색해 주세요. (ex: 명동)"
           />
+          <span className="absolute -translate-y-1/2 right-3 top-1/2">
+            {value && (
+              <button
+                type="button"
+                onClick={() => {
+                  onChange("");
+                  setOpen(false);
+                  setIsSelected(false);
+                }}
+                className="flex items-center justify-center"
+              >
+                <CloseIcon className="w-4 h-4 text-coolNeutral-40 hover:text-coolNeutral-20" />
+              </button>
+            )}
+          </span>
           {open && list.length > 0 && (
             <div className="absolute left-0 right-0 z-20 mt-2 overflow-hidden bg-white border rounded-lg shadow-md top-full border-coolNeutral-95">
               {list.map((item) => (
                 <button
                   key={item}
                   type="button"
-                  className="block w-full px-3 py-2 text-[12px] font-medium text-left text-coolNeutral-30 hover:text-blue-60 gap-[6px]"
+                  className="block w-full px-3 py-2 gap-[6px]
+                             text-[12px] font-medium text-left text-coolNeutral-30
+                             hover:text-blue-60"
                   onMouseDown={() => {
                     onChange(item);
                     setOpen(false);
@@ -95,8 +113,8 @@ export default function Step1({
               {open && debounced.trim().length >= 2 && list.length === 0 && (
                 <div
                   className="absolute left-0 right-0 top-full mt-2 z-20
-                rounded-[10px] border border-coolNeutral-90
-                bg-white px-3 py-2 text-sm text-coolNeutral-50 shadow-md"
+                             rounded-[10px] border border-coolNeutral-90
+                bg-white px-3 py-2 text-[12px] text-coolNeutral-50 shadow-md"
                 >
                   검색 결과가 없습니다.
                 </div>
