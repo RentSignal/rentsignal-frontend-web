589207a0eb552e0667f5bfe07be9403fd4f3e19c
WOOJIN CHOI <dnwls0723@sookmyung.ac.kr>
2026-06-21 15:44:08 +0900

    refactor: MainLayout 및 URL 서비스 코드 포맷 정리

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { Outlet, useLocation } from "react-router-dom";
 import Sidebar from "./Sidebar";
 import Map from "@/components/Map";
@@ -9,21 +9,28 @@ import RecommnedationResultPanel from "@/components/recommend/RecommendationResu
 
 const MainLayout = () => {
   const location = useLocation();
+
   const isHome = location.pathname === "/";
   const isCommunity = location.pathname.startsWith("/community");
 
-  //로고 헤더 숨길 경로들
-  const noHeaderPaths = ["/profile/name", "/profile/phone", "/profile/delete", "/profile/posts", "/profile/comments", "/profile/likes",];
+  // 로고 헤더 숨길 경로들
+  const noHeaderPaths = [
+    "/profile/name",
+    "/profile/phone",
+    "/profile/delete",
+    "/profile/posts",
+    "/profile/comments",
+    "/profile/likes",
+  ];
+
   const hideHeader = noHeaderPaths.includes(location.pathname);
 
   const [isOpen, setIsOpen] = useState(true);
-  const [loginOpen, setLoginOpen] = useState(false); //로그인 모달 상태
-  const [phoneOpen, setPhoneOpen] = useState(false); //전화번호 입력 모달 상태
-  const [recommendResultOpen, setRecommendResultOpen] = useState(false); //추천 결과 오른쪽 패널 상태관리
+  const [loginOpen, setLoginOpen] = useState(false);
+  const [phoneOpen, setPhoneOpen] = useState(false);
+  const [recommendResultOpen, setRecommendResultOpen] = useState(false);
   const [recommendResult, setRecommendResult] = useState<any>(null);
-  // useEffect(() => {
-  //   console.log("recommendResult:", recommendResult);
-  // }, [recommendResult]);
+
   return (
     <div className="flex h-screen overflow-hidden">
       {/* 60px Sidebar */}
@@ -35,6 +42,7 @@ const MainLayout = () => {
       <div className="relative flex-1">
         {/* 지도 */}
         <Map enableOverlay={isHome} />
+
         <RecommnedationResultPanel
           open={recommendResultOpen}
           onClose={() => setRecommendResultOpen(false)}
@@ -44,29 +52,29 @@ const MainLayout = () => {
         {/* 패널 */}
         <div
           className={`
-              absolute left-0 top-0 h-full ${isCommunity ? "w-fit" : "w-[377px]"}
-              bg-white border-r border-divider_grey
-              z-40
-              transition-transform duration-300 ease-in-out
-              ${isOpen ? "translate-x-0" : "translate-x-[-315px]"}
-            `}
+            absolute left-0 top-0 h-full ${isCommunity ? "w-fit" : "w-[377px]"}
+            bg-white border-r border-divider_grey
+            z-40
+            transition-transform duration-300 ease-in-out
+            ${isOpen ? "translate-x-0" : "translate-x-[-315px]"}
+          `}
         >
           {/* 헤더 */}
-          {!isCommunity && (
-          {/* 해당 경로에서만 헤더 숨김 */}
-          {!hideHeader && (
+          {!isCommunity && !hideHeader && (
             <PanelHeader onToggle={() => setIsOpen((prev) => !prev)} />
           )}
+
           <Outlet
             context={{
               openLoginModal: () => setLoginOpen(true),
               openPhoneModal: () => setPhoneOpen(true),
               openResultPanel: () => setRecommendResultOpen(true),
               setRecommendResult,
-              onToggle: () => setIsOpen((prev) => !prev)
+              onToggle: () => setIsOpen((prev) => !prev),
             }}
           />
         </div>
+
         <LoginModal open={loginOpen} onClose={() => setLoginOpen(false)} />
         <PhoneModal open={phoneOpen} onClose={() => setPhoneOpen(false)} />
       </div>
